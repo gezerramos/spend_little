c76ebf5c1eeb5ca0fcaf194673eb0ada86fd79c8
gezerramos2 <gezerramo@gmail.com>
2022-03-28 22:15:07 -0300

    teste5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,7 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
-      
+        if (!App::environment('local')) {
+            URL::forceSchema('https');
+       }
     }
 }
